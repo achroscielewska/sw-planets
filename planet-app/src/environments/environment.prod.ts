020f0f6f1d3a924da51272b8fab758f23448b734
Agnieszka Chróścielewska <agnieszka.chroscielewska@altkomsoftware.pl>
2020-03-11 17:58:14 +0100

    env : configuration added for production build

--- planet-app/src/environments/environment.prod.ts
+++ planet-app/src/environments/environment.prod.ts
@@ -1,3 +1,14 @@
+const API_URL = 'https://swapi.co/api';
+
 export const environment = {
+  planetsUrl: {
+    getList: `${API_URL}/planets`
+  },
+  peopleUrl: {
+    getList: `${API_URL}/people`
+  },
+  filmsUrl: {
+    getList: `${API_URL}/films`
+  },
   production: true
 };
